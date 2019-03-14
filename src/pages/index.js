5a11adc9149bef34e0c54fa383e199ccaf1126df
Christopher Stewart <christopherallenstewart@gmail.com>
2019-03-14 16:14:29 -0700

    stable with what I believe to be all the necessary npm biz

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { render } from 'react-dom';
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hayo.  This is a POC of De-coupled Drupal and Gatsby.</h1>
-    <p>It uses the GraphQL module on the Drupal side, then uses the Apollo client plugin in Gatsby on the frontend.</p>
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'http://drupalapollogatsbypoc.lndo.site/graphql' }),
+  cache: new InMemoryCache(),
+});
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    <Image />
-    </div>
-    <Link to="/stuffPage/">Go to Stuff Page</Link>
-  </Layout>
+
+const IndexPage = () => (
+  <ApolloProvider client={client}>
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Hayo.  This is a POC of De-coupled Drupal and Gatsby.</h1>
+      <p>It uses the GraphQL module on the Drupal side, then uses the Apollo client plugin in Gatsby on the frontend.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+        </div>
+        <div>test</div>
+      <Link to="/stuffPage/">Go to Stuff Page</Link>
+    </Layout>
+  </ApolloProvider>
 )
 
 export default IndexPage
--- src/pages/stuffPage.js
+++ src/pages/stuffPage.js
@@ -4,7 +4,6 @@ import StuffList from "../components/stuffList"
 
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const tempStuff = [
   {
