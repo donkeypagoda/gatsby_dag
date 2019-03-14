ec863baf0afa7a42e8dc229e71f0ad5ff5faf5b9
Christopher Stewart <christopherallenstewart@gmail.com>
2019-03-14 16:43:17 -0700

    added graphql-tag to update, no reworking to actually import

--- src/pages/stuffPage.js
+++ src/pages/stuffPage.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import StuffList from "../components/stuffList"
-
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag'
 
 import Layout from "../components/layout"
 
@@ -18,9 +19,25 @@ const tempStuff = [
   }
 ]
 
+const query = gql`
+  {
+    nodeQuery{
+      entities{
+        entityId
+      }
+    }
+  }
+`;
+
+const withQuery = graphql(query, {
+  props: ({ data: { nodeQuery } }) => ({
+    nodes: nodeQuery
+  }),
+});
+
 const Stuff = () => (
   <Layout>
-    <StuffList stuffs={tempStuff} />
+    <StuffList stuffs={withQuery} />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
