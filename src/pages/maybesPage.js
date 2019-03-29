e43d4f518d382a58089207056ec7395e5f91fbd6
Christopher Stewart <christopherallenstewart@gmail.com>
2019-03-29 09:04:40 -0700

    cleaning up unused variables

--- src/pages/maybesPage.js
+++ src/pages/maybesPage.js
@@ -2,9 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
-
 export const Maybes_QUERY = graphql`
     {
       dagGraphQL{
--- src/pages/stuffPage.js
+++ src/pages/stuffPage.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Img from "gatsby-image"
 
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
